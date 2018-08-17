80f9683baefaefb6df041706776f0003d3729533
Felipe Hernandez Palafox <felipe.sistemas.fhp@gmail.com>
2018-08-17 11:03:08 -0500

    Curso de POO en JAVA
    Curso de POO en JAVA

---
+++ app14/src/app14/App14.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app14;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author felipehernandez
+ */
+public class App14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("------------------------------");
+        String cadena = "Esta es mi cadena";
+        
+        System.out.println(cadena.charAt(0));
+        System.out.println(cadena.charAt(5));
+        System.out.println(cadena.charAt(cadena.length()-1));
+        
+        System.out.println("------------------------------");
+        
+        String nombreAlumno = "Juan Pablo";
+        
+        if (nombreAlumno.charAt(0) == 'J') {
+            System.out.println("La primer letra si es mayuscula");
+        } else {
+            System.out.println("La primer letra no es mayuscula");
+        }
+        
+        String saludo = "Buenas tardes!!";
+        
+        System.out.println(saludo.charAt(saludo.length()-3));
+        
+        
+        for (int i = 0; i < saludo.length(); i++) {
+            
+            char caracter  =  saludo.charAt(i);
+            
+            System.out.println(i+"->"+caracter);
+        }
+        
+        System.out.println("------------------------------");
+        
+        char caracteres[] = {'H','o','l','a'};
+        System.out.println(caracteres.length);
+        
+        String saludo2  = "";
+        
+        for (int i = 0; i < caracteres.length; i++) {
+            saludo2 += caracteres[i];
+        }
+        
+        System.out.println(saludo2);
+        
+        
+        
+        System.out.println("------------------------------");
+        
+        String cadena2  =  "Esto es una cadena dE CaracTEres";
+        String mayusc  = cadena2.toUpperCase();
+        String minusc = cadena2.toLowerCase();
+        
+        System.out.println("Original:"+cadena2);
+        System.out.println("Mayusculas:"+mayusc);
+        System.out.println("Minusculas:"+minusc);
+        
+        
+        System.out.println("------------------------------");
+        
+        String Cadena3 = "Los jovenes de ITSN esten en clase";
+        
+        int  posic1  = Cadena3.indexOf("ITSN");
+        int posic2  = Cadena3.lastIndexOf("Jo");
+        int posic3  = Cadena3.indexOf("ISSC");
+        
+        System.out.println(posic1);
+        System.out.println(posic2);
+        System.out.println(posic3);
+        
+        Scanner scanner  =  new Scanner(System.in);
+        
+        System.out.println("Capturame el caracter o string a buscar");
+        
+        String captura  = scanner.next();
+        
+        int posic4  =  Cadena3.indexOf(captura);
+        
+        if (posic4 != -1) {
+            System.out.println("Si se encuentra: "+posic4);
+        }else {
+            System.out.println("No se encontro");
+        }
+        
+    }
+    
+}
