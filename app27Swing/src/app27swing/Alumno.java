6fadab61be3f3de744dd19af5ad4f91b0a898ec3
Felipe Hernandez Palafox <felipe.sistemas.fhp@gmail.com>
2018-10-05 11:56:16 -0500

    Curso de POO en JAVA
    Curso de POO en JAVA

---
+++ app27Swing/src/app27swing/Alumno.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app27swing;
+
+/**
+ *
+ * @author felipehernandez
+ */
+public class Alumno {
+    
+    private  String nombre;
+    private  String matricula;
+    private  String telefono;
+    private  String carrera;
+    private  String hora;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+    
+    
+    
+    
+}
